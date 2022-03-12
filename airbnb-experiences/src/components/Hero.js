bf19369b33f5e30636a206bbe9793c3ca2e6ab80
Chandrabhan Singh <92934758+Chandra27D@users.noreply.github.com>
2022-03-12 12:32:37 +0530

    Add files via upload

---
+++ airbnb-experiences/src/components/Hero.js
@@ -0,0 +1,9 @@
+export default function Hero (){
+    return(
+        <section className="hero--wrapper">
+            <img src='../../images/photo-grid.png' alt="grid" id='hero--grid'/>
+            <h1 className='hero--title'>Online Experiences</h1>
+            <p className='hero--para'>Join unique interactive activities led by one-of-a-kind hosts-all without leaving home</p>            
+        </section>
+    )
+}
